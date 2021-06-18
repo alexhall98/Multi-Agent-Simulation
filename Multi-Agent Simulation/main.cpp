cbf72de40a337c21f4b4b53cbc8d0510f3e8545f
alexhall98 <ajhall129@gmail.com>
2021-06-18 14:19:50 +0100

    Initial Commit

---
+++ Multi-Agent Simulation/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Multi-Agent Simulation
+//
+//  Created by Alex on 18/06/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
